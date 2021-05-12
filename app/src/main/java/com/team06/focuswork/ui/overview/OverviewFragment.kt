4721614ab0127f10d9813e92d21b94998967719f
Anna Maria Pichler <anna.m.pichler@student.tugraz.at>
2021-05-12 11:47:38 +0200

    FW-035 small Refactoring [PA,ED]

--- app/src/main/java/com/team06/focuswork/ui/overview/OverviewFragment.kt
+++ app/src/main/java/com/team06/focuswork/ui/overview/OverviewFragment.kt
@@ -57,14 +57,11 @@ class OverviewFragment : Fragment() {
             currentTasks.addAll(tasks)
             (recyclerView.adapter as TaskAdapter).notifyDataSetChanged()
         })
-
-
     }
 
     private fun createNotifChannel() {
-        // based off this tutorial
+        // based on this tutorial
         // https://www.youtube.com/watch?v=B5dgmvbrHgs
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val name = "Timer finished"
             val descriptionText = "The timer for your task has finished."
@@ -81,7 +78,7 @@ class OverviewFragment : Fragment() {
     }
 
     private fun sendNotif(view: View) {
-        // based off this tutorial
+        // based on this tutorial
         // out first notification, navigates back to app by clicking on it
         // https://www.youtube.com/watch?v=B5dgmvbrHgs
         val intent = Intent(requireContext(), MainActivity::class.java).apply {
