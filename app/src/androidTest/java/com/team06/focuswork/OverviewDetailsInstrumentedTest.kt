43c75b75f5a404c85162e5231ff84e10374daee1
David Egger <david.egga@gmx.at>
2021-05-16 12:10:06 +0200

    FW-035 small fix in failing tests[ED,PA]

--- app/src/androidTest/java/com/team06/focuswork/OverviewDetailsInstrumentedTest.kt
+++ app/src/androidTest/java/com/team06/focuswork/OverviewDetailsInstrumentedTest.kt
@@ -1,13 +1,14 @@
 package com.team06.focuswork
 
 import android.view.Gravity
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.Espresso.pressBack
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.*
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.DrawerActions
 import androidx.test.espresso.contrib.DrawerMatchers.isClosed
 import androidx.test.espresso.contrib.NavigationViewActions
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
@@ -52,9 +53,9 @@ class OverviewDetailsInstrumentedTest {
             .check(matches(isDisplayed()))
         Thread.sleep(1000)
 
-        onView(withId(R.id.task_item))
-            .check(matches(isDisplayed()))
-            .perform(click())
+        onView(withId(R.id.recycler_view)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click())
+        )
 
         onView(withId(R.id.fragment_container_taskdetails))
             .check(matches(isDisplayed()))
